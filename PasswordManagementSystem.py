546560e30a97a286d0b2fac9933888547f162898
Tushant27 <tualoney27@gmail.com>
2026-06-08 23:16:04 +0530

    Project 2 Password Management System

---
+++ PasswordManagementSystem.py
@@ -0,0 +1,54 @@
+import random
+import string
+
+password = {}
+
+try : 
+    with open("password.txt", "r") as file :
+        for line in file:
+            website , pwd = line.strip().split(":")
+            password[website] = pwd
+
+except :
+    pass
+
+def generate_password() :
+    chars = string.ascii_letters + string.digits + "@#$%^&"
+    password = "".join(random.choice(chars) for _ in range(8))
+    return password
+
+
+while True :
+    print ("\n------PASSWORD MANAGEMENT SYSTEM------")
+    print ("1.save passowrd")
+    print ("2.View password")
+    print ("3.Generate password")
+    print ("4.Exit")
+
+    choice = input("Enter your choice : ")  
+
+    if choice == "1" :
+       site = input("enter website : ")
+       pwd = input("enter password : ")
+       password[site] = pwd             #save the "password" with given "key" i.e password{google:1231231}
+
+       with open("password.txt", "a") as file:
+          file.write(f"{site}:{pwd}\n")
+
+       print("Saved!")
+
+    elif choice == "2" :
+     if not password :
+           print ("no password") 
+     else :
+          for site,pwd in password.items():
+            print(site, ":" ,pwd)
+     
+
+    elif choice == "3":
+     print("Generated password : ",generate_password())  
+        
+
+
+    elif choice == "4":
+     print("Bye")       
